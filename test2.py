6e7576657886009a48da62d7b9fd806247d8701e
Anjalirajput837 <anjalirajput5051@gmail.com>
2025-11-13 14:41:04 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,5 +2,7 @@ number = int(input("Enter a number: "))
 
 if number % 2 == 0:
     print(f"{number} is an even number.")
+elif score >= 70:
+    print("Grade: C")
 else:
     print(f"{number} is an odd number.")
