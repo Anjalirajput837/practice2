0d1d686b23a950019929159be50acc3dbce73413
Anjali <anjalirajput5051@gmail.com>
2025-11-13 14:41:55 +0530

    Grade B

--- test2.py
+++ test2.py
@@ -2,5 +2,8 @@ number = int(input("Enter a number: "))
 
 if number % 2 == 0:
     print(f"{number} is an even number.")
+elif score >= 80:
+    print("Grade: B")
+
 else:
     print(f"{number} is an odd number.")
